0785e6af8112d993e35a77d4f8c26dd559d3a79e
avinash <avinash.srivastava@innovaccer.com>
2018-05-14 16:52:57 +0530

    Webpack demo.

---
+++ src/print.js
@@ -0,0 +1,9 @@
+export default function printMe() {
+  console.log('I get called from print.js!');
+  alert("Watch will automa");
+  alert("Watch will automa");
+  console.log("This is ");
+  console.log("VMVMVM");
+  console.log("A");
+  console.log("this");
+}
